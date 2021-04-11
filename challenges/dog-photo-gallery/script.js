cfd6e2e2885a2e63c2cf9c278e9dd2b128847c35
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-04-11 21:15:30 +0200

    dog-photo-gallery - javascipt complete

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,1 +1,21 @@
-let button = document.querySelector(input);
+let targetButton = document.getElementById("btn");
+// console.log(targetButton);
+targetButton.addEventListener("click", getDogImages);
+
+let listItem = document.getElementById("listItem")
+// console.log(listItem);
+listItem.style.listStyle = "none"
+
+let imageItem = document.getElementById("imageItem")
+// console.log(imageItem);
+
+function getDogImages() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+        .then(data => {
+            console.log("data =", data.message);
+            imageItem.src = data.message
+        }
+    ) 
+    .catch(error => console.log(`Oh no ${error}`));    
+}
